03367c0147852280a12eda457c9dc0a7acbdf6c3
swakshantayade <swakshan01022003@gmail.com>
2024-07-24 16:17:52 +0530

    index updated

--- index.js
+++ index.js
@@ -24,9 +24,9 @@ app.use(cors({
 app.use(fileUpload({
     useTempFiles:true
 }))
+app.set('views', path.join(path.resolve(),"views"))
 app.set('view engine','ejs')
 
-app.set('views', path.join(path.resolve(),"views"))
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 // app.use(express.static('public'))
